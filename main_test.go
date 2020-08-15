d51f7b7957812e25f29ba59e7cb42ddccd60d44b
George Chen <gchen@datomar.com>
2020-08-15 18:41:56 -0500

    add message assertion

--- main_test.go
+++ main_test.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
+	"strconv"
+	"strings"
 	"testing"
 )
 var counter =make(chan int64)
@@ -33,6 +37,50 @@ func TestMain(m *testing.M) {
 
 	//clean redis cache
 	redisClient.ClusterResetHard()
+	
+	AssertOutputCorrectness(output)
+}
+
+func AssertOutputCorrectness(dir string) {
+	files, _ := filepath.Glob(filepath.Join(dir, "*"))
+	if len(files) != settings.CorrelationCount {
+		log.Fatalf("number of output files not match, expected: %d, got: %d", settings.CorrelationCount, len(files))
+	}
+
+	for _, o := range files {
+		count := scanFile(o)
+		if count != settings.SessionSize {
+			log.Fatalf("number of message for %s is incorrect, expected: %d, got: %d", o, settings.SessionSize, count)
+		}
+	}
+
+}
+func scanFile(name string) (int) {
+	f, err := os.OpenFile(name, os.O_RDONLY, os.ModePerm)
+	if err != nil {
+		log.Fatalf("open file error: %v", err)
+		return 0
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner(f)
+	count :=0
+	s :=0
+	for sc.Scan() {
+		l := sc.Text()
+		c := strings.Split(l,":")
+		if  seqNum, _ := strconv.Atoi(c[1]); seqNum != s {
+			log.Fatalf("%s: Messages outof order!", name)
+		}
+		count++
+		s++
+	}
+	if err := sc.Err(); err != nil {
+		log.Fatalf("scan file error: %v", err)
+		return 0
+	}
+
+	return count
 }
 
 func BenchmarkProcessThreads(b *testing.B) {
